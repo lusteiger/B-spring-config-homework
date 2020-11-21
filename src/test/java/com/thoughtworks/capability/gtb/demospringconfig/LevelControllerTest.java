3e2563a9edc5acfc47bc9c749940466181898602
lusteiger <leonardoxy7@gmail.com>
2020-11-21 17:42:18 +0800

    [xinyu lu] solution 3

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
@@ -5,33 +5,34 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class LevelControllerTest {
 
-    @SpringBootTest(properties = { "levelNumber=0" })
+    @SpringBootTest
     @Nested
     @AutoConfigureMockMvc
+    @TestPropertySource(locations = "classpath:application-0.properties")
     class when_level_number_is_0 {
         @Autowired
         MockMvc mockMvc;
 
         @Test
         public void should_return_basic() throws Exception {
-
             mockMvc.perform(get("/level"))
                     .andExpect(content().string("basic"));
         }
     }
-    @SpringBootTest(properties = { "levelNumber=1" })
+
+    @SpringBootTest
     @Nested
     @AutoConfigureMockMvc
+    @TestPropertySource(locations = "classpath:application-1.properties")
     class when_level_number_is_1 {
         @Autowired
         MockMvc mockMvc;
@@ -44,9 +45,10 @@ class LevelControllerTest {
         }
     }
 
-    @SpringBootTest(properties = { "levelNumber=2" })
+    @SpringBootTest
     @Nested
     @AutoConfigureMockMvc
+    @TestPropertySource(locations = "classpath:application-2.properties")
     class when_level_number_is_2 {
         @Autowired
         MockMvc mockMvc;
